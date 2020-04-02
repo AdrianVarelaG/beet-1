f80d717c30205367ab3f737be481ffdfbc2f304f
Cesar Varela <cesarac66@hotmail.com>
2020-04-02 12:22:29 -0600

    Add Runtime config for KEYCLOAK

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,11 @@
 FROM nginx:stable-alpine
+RUN apk add --no-cache bash
+RUN rm -rf /etc/nginx/conf.d
+COPY conf /etc/nginx
 COPY /build /usr/share/nginx/html
 EXPOSE 80
-CMD ["nginx", "-g", "daemon off;"]
+WORKDIR /usr/share/nginx/html
+COPY ./env.sh .
+COPY .env .
+RUN chmod +x env.sh
+CMD ["/bin/bash", "-c", "/usr/share/nginx/html/env.sh && nginx -g \"daemon off;\""]
--- src/components/ExploreContainer.tsx
+++ src/components/ExploreContainer.tsx
@@ -6,6 +6,9 @@ interface ContainerProps { }
 const ExploreContainer: React.FC<ContainerProps> = () => {
   return (
     <div className="container">
+      <p>KEYCLOAK_URL: {(window as any)._env_?.KEYCLOAK_URL}</p>
+      <p>KEYCLOAK_REALM: {(window as any)._env_?.KEYCLOAK_REALM}</p>
+      <p>KEYCLOAK_CLIENT_ID: {(window as any)._env_?.KEYCLOAK_CLIENT_ID}</p>
       <strong>Ready to create an app?</strong>
       <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
     </div>
